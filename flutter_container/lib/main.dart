27378822756676e35d3d8ba149918e34ef9678f2
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-11-10 21:58:21 +0530

    How the Center widget works

--- flutter_container/lib/main.dart
+++ flutter_container/lib/main.dart
@@ -15,13 +15,15 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           body: Container(
-            margin: const EdgeInsets.all(40), //margin for the all direction
+            // margin: const EdgeInsets.all(40), //margin for the all direction
             // margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-            padding: const EdgeInsets.all(30), //affect to the child
+            // padding: const EdgeInsets.all(30), //affect to the child
             width: 200,
             height: 200,
             color: Colors.blue,
-            child: const Text("Flutter"),
+            child: const Center(
+              child: Text("Flutter"),
+            ),
           ),
         ),
       ),
