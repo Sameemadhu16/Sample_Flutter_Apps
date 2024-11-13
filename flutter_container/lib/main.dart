bdcf399011dd28413e7c065b3bef279c3f7b94de
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-11-13 12:57:58 +0530

    How main and cross alignments works

--- flutter_container/lib/main.dart
+++ flutter_container/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,31 +9,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Row(
-            children: [
-              Expanded(
-                child: Text(
+          body: Container(
+            width: 300,
+            height: 300,
+            color: Colors.yellow,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
                   "Flutter is easy",
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 40),
                 ),
-              ),
-              Expanded(
-                child: Text(
+                Text(
                   "Dart is easy",
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 40),
                 ),
-              ),
-              Expanded(
-                child: Icon(
+                Icon(
                   Icons.restaurant,
                   size: 40,
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
